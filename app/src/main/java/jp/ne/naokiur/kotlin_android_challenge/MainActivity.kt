e49832e514f32b7b17ec660f70f910dabaeddf9c
naokiur <atomic.transfer.comp@gmail.com>
2017-10-28 17:28:16 +0900

    Add Network access button.

--- app/src/main/java/jp/ne/naokiur/kotlin_android_challenge/MainActivity.kt
+++ app/src/main/java/jp/ne/naokiur/kotlin_android_challenge/MainActivity.kt
@@ -2,6 +2,8 @@ package jp.ne.naokiur.kotlin_android_challenge
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         connect_network.text = getString(R.string.button_network_label)
-        connect_network.setOnClickListener {  }
+        connect_network.setOnClickListener { view ->
+            connect_network.text = "After click."
+
+            "https://api.github.com/repos/naokiur/kotlin-android-challenge".httpGet().responseString { request, responce, result ->
+
+                when (result) {
+                    is Result.Failure -> {
+                        print(result)
+                    }
+
+                    is Result.Success -> {
+                        network_result.text = result.toString()
+                    }
+                }
+            }
+        }
+
+
+
     }
 }
