1efa4b58184f21c6d689018cf334cddb8f3274f2
naokiur <atomic.transfer.comp@gmail.com>
2017-10-28 16:01:07 +0900

    Add button to MainActivity.

--- app/src/main/java/jp/ne/naokiur/kotlin_android_challenge/MainActivity.kt
+++ app/src/main/java/jp/ne/naokiur/kotlin_android_challenge/MainActivity.kt
@@ -1,12 +1,16 @@
 package jp.ne.naokiur.kotlin_android_challenge
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connect_network.text = getString(R.string.button_network_label)
+        connect_network.setOnClickListener {  }
     }
 }
