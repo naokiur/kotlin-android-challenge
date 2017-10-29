7e5348393c783a7103a628a029a9f9a74692acbb
naokiur <atomic.transfer.comp@gmail.com>
2017-10-29 16:18:36 +0900

    Display local directory and file on DialogFragment by ListView.

--- app/src/main/java/jp/ne/naokiur/kotlin_android_challenge/DirectoryDialog.kt
+++ app/src/main/java/jp/ne/naokiur/kotlin_android_challenge/DirectoryDialog.kt
@@ -3,20 +3,35 @@ package jp.ne.naokiur.kotlin_android_challenge
 import android.app.AlertDialog
 import android.app.Dialog
 import android.app.DialogFragment
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import java.io.File
+
 
 /**
  * Created by nao-ur on 2017/10/29.
  */
 class DirectoryDialog: DialogFragment() {
-    private val title = "title"
-    private val msg = "message"
+    private val title = "Directory List"
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return activity.layoutInflater.inflate(R.layout.directory_fragment_layout, null)
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(activity)
+        val list = File(activity.filesDir, "").walkTopDown().toList()
 
-        builder.setTitle(title)
-                .setMessage(msg)
+        val adapter: ArrayAdapter<File> = ArrayAdapter(activity, android.R.layout.select_dialog_item, list)
+
+        val builder = AlertDialog.Builder(activity)
+        builder.setView(activity.layoutInflater.inflate(R.layout.directory_fragment_layout, null))
+                .setTitle(title)
+                .setAdapter(adapter, DialogInterface.OnClickListener { dialog, which ->
+                })
 
         return builder.create()
     }
